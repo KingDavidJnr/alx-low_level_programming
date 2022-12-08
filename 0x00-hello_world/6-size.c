9d564ed52c82b9dad848332b67f6d95158ac40df
David Oduse <odusedavid@gmail.com>
2022-12-08 12:30:59 -0800

    New Correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ long long alonglong;
 float afloat;
 
 printf("Size of a char: %zu byte(s)\n", (unsigned long) sizeof(acharacter));
-printf("size of an int: %zu byte(s)\n", (unsigned long) sizeof(ainteger));
+printf("size of an int: %zu byte(s)\n", sizeof(ainteger));
 printf("Size of a long int: %zu byte(s)\n", (unsigned long) sizeof(along));
 printf("Size of a long long int: %zu byte(s)\n", (unsigned long) sizeof(alonglong));
 printf("Size of a float: %zu byte(s)\n", (unsigned long) sizeof(afloat));
