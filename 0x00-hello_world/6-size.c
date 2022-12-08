a6ba722acc5e9a7f5264337dac2df10c33a58a61
David Oduse <odusedavid@gmail.com>
2022-12-08 13:28:29 -0800

    Fix and fixings

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,8 +16,8 @@ float afloat;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("size of an int: %zu byte(s)\n", sizeof(int));
-printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 return (0);
