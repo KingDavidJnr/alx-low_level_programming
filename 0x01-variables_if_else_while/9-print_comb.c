1e523dec9718a73a802e9ddd3ab58cfe4b975119
David Oduse <odusedavid@gmail.com>
2022-12-09 15:16:48 -0800

    Fifth Correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,10 @@ int number;
 for (number = 0; number <= 9; number++)
 {
 putchar(number + '0');
-if (number < 9)
+if (number <= 9)
 {
 putchar(',');
 putchar(' ');
-}
-else (number = 9)
-{
-putchar(',');
 }
 }
 
