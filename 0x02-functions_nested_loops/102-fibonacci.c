e14c6504055c076438afe13344869ff932fba49f
David Oduse <odusedavid@gmail.com>
2022-12-13 15:42:52 -0800

    Fixing of task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 int i;
-int n;
 
 int t1 = 1;
 int t2 = 2;
@@ -18,7 +17,7 @@ int nextTerm = t1 + t2;
 
 printf("%d, %d, ", t1, t2);
 
-for (i = 1; n <= 50; ++i)
+for (i = 1; i <= 50; ++i)
 {
 printf("%d, ", nextTerm);
 t1 = t2;
