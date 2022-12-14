337b01f3f243f9944fd432694ec421154b275e0a
David Oduse <odusedavid@gmail.com>
2022-12-13 16:26:31 -0800

    Testing the waters

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests function prints whether integer
+ * ...is positve or negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
