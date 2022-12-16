9428c6939cb502ce15369fcbba82e76aa7fe696c
David Oduse <odusedavid@gmail.com>
2022-12-15 16:52:44 -0800

    Fourth fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,7 +14,7 @@ if (size <= 0)
 _putchar('\n');
 
 for (row = 0; row < size; row++)
-{
+
 for (column = size - row; column > 1; column--)
 _putchr(' ');
 
@@ -22,6 +22,5 @@ for (k = row + column; k >= 1; k--)
 _putchar('#');
 
 _putchar('\n');
-}
 
 }
