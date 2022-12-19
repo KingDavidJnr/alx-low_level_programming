247a0275caf7183ed59eab244dc3001f8c7431db
David Oduse <odusedavid@gmail.com>
2022-12-19 09:40:47 -0800

    9th fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,32 +10,26 @@
 void rev_string(char *s)
 {
 
-char *t;
-int i, len, len1;
+int len = 0;
+int l = 0;
+char *y = s;
+int e = 0;
+int x;
+char n;
 
-len = 0;
-len1 = 0;
-
-while (s[len] != '\0')
+while (*y != '\0')
 {
+y++;
 len++;
 }
 
 len1 = len - 1;
 
-<<<<<<< HEAD
 for (e < ((1 / 2) + 1) e++)
 {
 x = (l - e);
 n = s[e];
 s[e] = s[x];
 s[x] = n;
-=======
-for (i = 0; i < len / 2; i++)
-{
-*t = s[i];
-s[i] = s[len1];
-s[len1--] = *t;
->>>>>>> cbc4dc9a1c592cbfd258dc49bfea54f6384820b8
 }
 }
