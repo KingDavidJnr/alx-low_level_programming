8655aa82ef80f32e334e966102d5faf142e93632
David Oduse <odusedavid@gmail.com>
2022-12-19 09:59:29 -0800

    7th commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - print pair values
+ * @str: value to be evauated
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+
+int len = 0;
+int l = 0;
+char *y = str;
+int z;
+
+while (*y != '\0')
+{
+y++;
+len++;
+}
+
+for (z = 0; z <= l; z++)
+{
+if (z % 2 == 0)
+{
+_putchar(str[z]);
+}
+
+}
+
+_putchar('\n');
+}
