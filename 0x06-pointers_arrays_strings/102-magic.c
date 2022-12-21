e8a8bc2fa423fdc87022046bbeaa74fa7297ebe2
David Oduse <odusedavid@gmail.com>
2022-12-21 14:29:32 -0800

    3rd fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+
 int main(void)
 
 {
@@ -11,7 +17,7 @@ int *p;
 a[2] = 1024;
 p = &n;
 
-/*
+/**
 * write your line of code here...
 * Remember:
 * - you are not allowed to use a
