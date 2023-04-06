d2aa2d636d9cef2c3ec1658f33149abe938ff0a5
Oduse David <89360467+KingDavidJnr@users.noreply.github.com>
2023-04-06 17:55:33 +0000

    6-hash file refactor

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,8 +2,8 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * hash_table_delete - Removes the hash table.
+ * @ht: The pointer to hash table.
  */
 void hash_table_delete(hash_table_t *ht)
 {
